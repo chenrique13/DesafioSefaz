c24102de33343927672bb51129f5bf4dfb879a0f
Carlos Henrique do Nascimento Pereira <c.henrique1309@gmail.com>
2020-01-29 18:44:50 -0300

    Criando Metodo POST

--- Desafio/src/main/java/com/carlospereira/desafiosefaz/services/UsuarioService.java
+++ Desafio/src/main/java/com/carlospereira/desafiosefaz/services/UsuarioService.java
@@ -1,12 +1,12 @@
 package com.carlospereira.desafiosefaz.services;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.carlospereira.desafiosefaz.domain.Usuario;
 import com.carlospereira.desafiosefaz.repositories.UsuarioRepository;
-
-import java.util.Optional;
 
 @Service
 public class UsuarioService {
@@ -17,5 +17,10 @@ public class UsuarioService {
 	public Usuario buscar(Integer id) {
 		Optional<Usuario> obj = repositorio.findById(id);
 		return obj.orElse(null);
+	}
+	
+	public Usuario insert(Usuario obj) {
+		obj.setId(null);
+		return repositorio.save(obj);
 	}
 }
