05c8e95b58eecf0ddd71bafd4a49768493e13b93
Carlos Henrique do Nascimento Pereira <c.henrique1309@gmail.com>
2020-01-29 17:20:24 -0300

    Adicionando no banco h2
    Adicionando  Usuarios e telefones no banco h2

--- Desafio/src/main/java/com/carlospereira/desafiosefaz/DesafioApplication.java
+++ Desafio/src/main/java/com/carlospereira/desafiosefaz/DesafioApplication.java
@@ -7,14 +7,18 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.carlospereira.desafiosefaz.domain.Telefone;
 import com.carlospereira.desafiosefaz.domain.Usuario;
+import com.carlospereira.desafiosefaz.repositories.TelefoneRepository;
 import com.carlospereira.desafiosefaz.repositories.UsuarioRepository;
 
 @SpringBootApplication
-public class DesafioApplication implements CommandLineRunner{
-	
+public class DesafioApplication implements CommandLineRunner {
+
 	@Autowired
 	private UsuarioRepository usuarioRepository;
+	@Autowired
+	private TelefoneRepository telefoneRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(DesafioApplication.class, args);
@@ -22,14 +26,26 @@ public class DesafioApplication implements CommandLineRunner{
 
 	@Override
 	public void run(String... args) throws Exception {
-		
+
+		//criando usuarios
 		Usuario usuario1 = new Usuario(null, "Carlos", "c.henrique1309@gmail.com", "1234");
 		Usuario usuario2 = new Usuario(null, "Henrique", "henrique@gmail.com", "1234");
+
+		//criando telefones
 		
-		//Salvando usuarios no banco
+		Telefone telefone1 = new Telefone(null, 81, "85740926", "Celular", usuario1);
+		Telefone telefone2 = new Telefone(null, 81, "34430926", "Residencial", usuario1);
+		Telefone telefone3 = new Telefone(null, 81, "95562199", "Celular", usuario2);
 		
-		usuarioRepository.saveAll(Arrays.asList(usuario1,usuario2));
+		//associando telefones nos usuarios
 		
+		usuario1.getTelefones().addAll(Arrays.asList(telefone1,telefone2));
+		usuario2.getTelefones().addAll(Arrays.asList(telefone3));
+		
+		// Salvando usuarios e telefones no banco
+
+		usuarioRepository.saveAll(Arrays.asList(usuario1, usuario2));
+		telefoneRepository.saveAll(Arrays.asList(telefone1,telefone2,telefone3));
 	}
 
 }
