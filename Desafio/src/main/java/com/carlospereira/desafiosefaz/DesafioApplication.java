528e557931122b9dfb4724d7a235f94368808831
Carlos Henrique do Nascimento Pereira <c.henrique1309@gmail.com>
2020-01-29 15:46:22 -0300

    Adicionando usuarios no Banco h2

--- Desafio/src/main/java/com/carlospereira/desafiosefaz/DesafioApplication.java
+++ Desafio/src/main/java/com/carlospereira/desafiosefaz/DesafioApplication.java
@@ -1,13 +1,35 @@
 package com.carlospereira.desafiosefaz;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class DesafioApplication {
+import com.carlospereira.desafiosefaz.domain.Usuario;
+import com.carlospereira.desafiosefaz.repositories.UsuarioRepository;
 
+@SpringBootApplication
+public class DesafioApplication implements CommandLineRunner{
+	
+	@Autowired
+	private UsuarioRepository usuarioRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(DesafioApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Usuario usuario1 = new Usuario(null, "Carlos", "c.henrique1309@gmail.com", "1234");
+		Usuario usuario2 = new Usuario(null, "Henrique", "henrique@gmail.com", "1234");
+		
+		//Salvando usuarios no banco
+		
+		usuarioRepository.saveAll(Arrays.asList(usuario1,usuario2));
+		
 	}
 
 }
