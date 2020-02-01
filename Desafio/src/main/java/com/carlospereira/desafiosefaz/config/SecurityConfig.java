60e06c39c0ff90e77ea8edc84ddd9af5de7ff905
Carlos Henrique do Nascimento Pereira <c.henrique1309@gmail.com>
2020-02-01 01:01:24 -0300

    Prints detestes com Postman

---
+++ Desafio/src/main/java/com/carlospereira/desafiosefaz/config/SecurityConfig.java
@@ -0,0 +1,46 @@
+package com.carlospereira.desafiosefaz.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	private Environment environment;
+	private static final String[] PUBLIC_MACHERS = { "/usuarios/**", "/telefones/**", "/h2-console/**" };
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		if (Arrays.asList(environment.getActiveProfiles()).contains("test")) {
+			http.headers().frameOptions().disable();
+		}
+		http.cors().and().csrf().disable();
+		http.authorizeRequests().antMatchers(PUBLIC_MACHERS).permitAll().anyRequest().authenticated();
+	}
+
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+		return source;
+	}
+
+	@Bean
+	public BCryptPasswordEncoder codificadorDeSenha() {
+		return new BCryptPasswordEncoder();
+	}
+
+}
