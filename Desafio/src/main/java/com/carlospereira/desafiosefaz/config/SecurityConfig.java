ecfbfe5ec88fcf9ebe6b907fdf77b8da11d21ac3
Carlos Henrique do Nascimento Pereira <c.henrique1309@gmail.com>
2020-02-01 18:15:58 -0300

    Update SecurityConfig.java

--- Desafio/src/main/java/com/carlospereira/desafiosefaz/config/SecurityConfig.java
+++ Desafio/src/main/java/com/carlospereira/desafiosefaz/config/SecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -20,7 +21,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
 	private Environment environment;
-	private static final String[] PUBLIC_MACHERS = { "/usuarios/**", "/telefones/**", "/h2-console/**" };
+
+	private static String[] PUBLIC_MACHERS = { "/h2-console/**" };
+	// Liberando so para consulta
+	private static String[] PUBLIC_MACHERS_GET = { "/usuarios/**", "/telefones/**" };
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
@@ -28,7 +32,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			http.headers().frameOptions().disable();
 		}
 		http.cors().and().csrf().disable();
-		http.authorizeRequests().antMatchers(PUBLIC_MACHERS).permitAll().anyRequest().authenticated();
+		http.authorizeRequests().antMatchers(org.springframework.http.HttpMethod.GET, PUBLIC_MACHERS_GET).permitAll()
+				.antMatchers(PUBLIC_MACHERS).permitAll().anyRequest().authenticated();
+		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 
 	@Bean
